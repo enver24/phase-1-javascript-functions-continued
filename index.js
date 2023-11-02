74d6f4fc542d87cfd6dae0793a70202b31377291
Enver <mr_epsty@hotmail.com>
2023-11-02 16:40:21 +1100

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function(work = "go to the office") {
+    return `This Monday, I will ${work}.`;
+}
+
+function wrapAdjective(visual = "*") {
+    return function(sp = "special") {
+        return `You are ${visual}${sp}${visual}!`
+    }
+}
